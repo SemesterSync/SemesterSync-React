a606aed178124461199736e1fece3fc706009384
Tobias <61364477+tobias-hay@users.noreply.github.com>
2026-09-14 10:05:49 -0400

    chore: remove extra code from create modal

--- src/components/sidebar/courseAdd/course-add.tsx
+++ src/components/sidebar/courseAdd/course-add.tsx
@@ -1,11 +1,8 @@
-import { getAllCoursesWithMeetings } from "@/data/courses";
 import { getTerms } from "@/data/terms";
 import EventAddModalClient from "./event-add-client";
 
 export default async function CourseAddModal() {
 	const terms = await getTerms();
-	const meetings = await getAllCoursesWithMeetings();
 
-	return <EventAddModalClient termsRes={terms} courses={meetings} />;
-	// return <CourseAddModalClient termsRes={terms} courses={meetings} />;
+	return <EventAddModalClient termsRes={terms} />;
 }
