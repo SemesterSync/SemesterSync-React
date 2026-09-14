77e3b8909b0fae244d202416c7d0a0ad05b7ceb3
Tobias <61364477+tobias-hay@users.noreply.github.com>
2026-09-14 14:31:01 -0400

    chore: remove old term code

--- src/data/terms.ts
+++ src/data/terms.ts
@@ -1,26 +1,8 @@
 import "server-only";
 
 import { db } from "@/db/connection";
-import { type TermTableResult, termTable } from "@/db/schemas/terms";
+import { termTable } from "@/db/schemas/terms";
 import type { RevisedTermResponse } from "@/types/courses";
-
-type TermList = Array<TermTableResult>;
-
-type TermError = number;
-
-export type TermResponse = TermList | TermError;
-
-export async function getTerms(): Promise<TermResponse> {
-	try {
-		const data = await db.select().from(termTable);
-
-		return data;
-	} catch (error) {
-		console.error(error);
-
-		return -1;
-	}
-}
 
 export async function getAllTerms(): Promise<RevisedTermResponse[]> {
 	try {
